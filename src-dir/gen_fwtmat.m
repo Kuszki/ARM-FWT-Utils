eea7eaecf44cdbd74a21ffb866e7dfa3b0f41bbc
Kuszki <l.drozdz@o2.pl>
2025-06-28 11:00:29 +0200

    Update struct

---
+++ src-dir/gen_fwtmat.m
@@ -0,0 +1,10 @@
+pkg load ltfat
+pkg load parallel
+
+save_matrix('db2', 5, 128)
+save_matrix('db4', 4, 128)
+save_matrix('sym2', 5, 128)
+save_matrix('sym4', 4, 128)
+
+save_matrix('coif5', 3, 256)
+save_matrix('spline4:4', 3, 256)
--- src-dir/save_matrix.m
+++ src-dir/save_matrix.m
@@ -1,7 +1,7 @@
 function A = save_matrix(name, lvl, len)
 
 	A = lin_ident(@(x) fwt(x, name, lvl), len);
-	N = sprintf("../data-dir/%s_%d_%d.dat", name, lvl, len);
+	N = sprintf("../data-dir/%s_%d_%d.hdf", name, lvl, len);
 
 	save("-hdf5", N, "A");
 
