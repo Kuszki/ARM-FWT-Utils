842399bffd5e7285ce96d8a937b3453195dd5aea
Kuszki <l.drozdz@o2.pl>
2025-06-28 11:15:14 +0200

    Update CMake

--- src-dir/gen_fwtmat.m
+++ src-dir/gen_fwtmat.m
@@ -1,10 +1,12 @@
-pkg load ltfat
-pkg load parallel
+addpath("../fwt-utils");
 
-save_matrix('db2', 5, 128)
-save_matrix('db4', 4, 128)
-save_matrix('sym2', 5, 128)
-save_matrix('sym4', 4, 128)
+pkg load ltfat;
+pkg load parallel;
 
-save_matrix('coif5', 3, 256)
-save_matrix('spline4:4', 3, 256)
+save_matrix('db2', 5, 128);
+save_matrix('db4', 4, 128);
+save_matrix('sym2', 5, 128);
+save_matrix('sym4', 4, 128);
+
+save_matrix('coif5', 3, 256);
+save_matrix('spline4:4', 3, 256);
